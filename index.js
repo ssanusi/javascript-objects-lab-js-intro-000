49132740a290fdc388be89f6a7ec619003cd9bda
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-21 01:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
+  var clone = Object.assign({},object,{key: value})
   return object;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
