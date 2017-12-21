5b0f9a5b0c5797559099e49719d8e7766e5f8507
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-21 01:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
+  return object;
 }
 function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({},object);
