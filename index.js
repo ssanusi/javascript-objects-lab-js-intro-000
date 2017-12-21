235957ab1bc5e5bc022c51469f5da450e7ff2e35
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-21 00:59:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
-
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
 }
