61e656dc547c8d9b872d0a06273378f2afceaaf8
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-21 00:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey(object, key) {
   delete object.key;
-  
+
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
